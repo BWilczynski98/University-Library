0dda6972bdb09655e5c34579a528606bacbe4a5c
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2024-07-04 16:06:42 +0200

    Authorization and authentication

---
+++ backend/core/security.py
@@ -0,0 +1,31 @@
+import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from decouple import config
+
+from models.user import Users
+
+SECRET_KEY = config("secret")
+ALGORITHM = str(config("algorithm"))
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def authenticate_user(email: str, password: str, db):
+    user = db.query(Users).filter(Users.email == email).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
